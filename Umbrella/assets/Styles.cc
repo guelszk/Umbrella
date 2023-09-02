5ebe9863ac02220f6963008be097c55f8209168d
guelszk <143238355+guelszk@users.noreply.github.com>
2023-09-01 23:04:49 -0300

    Add files via upload

---
+++ Umbrella/assets/Styles.cc
@@ -0,0 +1,12 @@
+
+  @import url('https://fonts.googleapis.com/css2?family=Golos+Text&family=Open+Sans:ital,wght@1,300&display=swap');
+
+
+.sinopse{
+
+font-family: 'Open Sans', sans-serif;
+color:#FF0000
+text-align:center
+
+}
+
